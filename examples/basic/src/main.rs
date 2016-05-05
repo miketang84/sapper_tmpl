24f88c597ce22e80eef1e75197182c9a51847a95
Mike Tang <miketang84@outlook.com>
2016-05-05 17:36:19 +0800

    work.
    Signed-off-by: Mike Tang <miketang84@outlook.com>

---
+++ examples/basic/src/main.rs
@@ -0,0 +1,50 @@
+
+extern crate sapper;
+extern crate sapper_tmpl;
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
+use sapper::{SApp, SAppWrapper, Request, Response, Result};
+
+
+
+
+mod biz;
+use biz::Biz;
+
+
+
+#[derive(Clone)]
+struct MyApp;
+// must impl it
+// total entry and exitice
+impl SAppWrapper for MyApp {
+    fn before(&self, req: &mut Request) -> Result<()> {
+        println!("{}", "in SAppWrapper before.");
+        
+        Ok(())
+    }
+    
+    fn after(&self, req: &Request, res: &mut Response) -> Result<()> {
+        println!("{}", "in SAppWrapper after.");
+        
+        Ok(())
+    }
+}
+
+
+
+pub fn main() {
+    env_logger::init().unwrap();
+    
+    let mut sapp = SApp::new();
+    sapp.address("127.0.0.1")
+        .port(1337)
+        .with_wrapper(MyApp)
+        .add_module(Biz);
+    
+    println!("Listening on http://127.0.0.1:1337");
+    sapp.run();
+    
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate tera;
+
+use tera::Tera;
+pub use tera::Context;
+
+lazy_static! {
+    static ref TERA: Tera = Tera::new("views/**/*");
+}
+
+pub fn render(path: &str, context: Context) -> String {
+    TERA.render(path, context).unwrap_or_else(|e| {
+        println!("rendering error: {:?}", e);
+        // warn!("rendering error: {:?}", e);
+        "rendering error".to_owned()
+    })
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
