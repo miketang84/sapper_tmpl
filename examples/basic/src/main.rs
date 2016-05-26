2bc667532e051fb1c8b7598f57f367ab6b5ea61b
Mike Tang <miketang84@outlook.com>
2016-05-26 19:21:02 +0800

    update to latest sapper.
    Signed-off-by: Mike Tang <miketang84@outlook.com>

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -38,15 +38,11 @@ impl SAppWrapper for MyApp {
 pub fn main() {
     env_logger::init().unwrap();
 
-    fn init_global(req: &mut Request) -> Result<()> {
-        Ok(())
-    }
     
     let mut sapp = SApp::new();
     sapp.address("127.0.0.1")
         .port(1337)
-        .init_global(init_global)
-        .with_wrapper(MyApp)
+        .with_wrapper(Box::new(MyApp))
         .add_module(Box::new(Biz));
     
     println!("Listening on http://127.0.0.1:1337");
